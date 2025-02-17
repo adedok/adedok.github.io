0011f1fca26c98adccf7ef1d2b56b3cc1611d05d
chenxiaoshuai <chenxiaoshuai@apusapps.com>
2025-02-17 17:20:14 +0800

    最新版UI

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,15 @@
 <script setup>
+import ImgHL from '@/assets/image/h-l.png'
+import ImgLogoB from '@/assets/image/logo-b.png'
+import ImgHCT from '@/assets/image/h-c-t.png'
+import ImgHCB from '@/assets/image/h-c-b.png'
+import ImgWQcH from '@/assets/image/wrap-qc-h.png'
+import ImgWCLi1 from '@/assets/image/wrap-c-li.png'
+import ImgWCLi2 from '@/assets/image/wrap-c-li2.png'
+import ImgWQcB from '@/assets/image/wrap-qc-b.png'
+
+
+// h-c-b
 const open = (type) => {
   if(type == 'privacy'){
     window.open('./privacy.html','_target')
@@ -6,35 +17,68 @@ const open = (type) => {
     window.open('./terms.html', '_target')
   }
 }
+
 </script>
 
 <template>
   <div class="wrap">
-    <div class="wrap-con">
-      <div class="wrap-con-top">
-        <img src="@/assets/image/top.png">
-      </div>
-      <div class="wrap-con-t">
-        <div class="left">
-          <img class="graph" src="@/assets/image/icon.png">
-          <p class="tit">Enjoy <span>FortiShield</span></p>
-          <p class="desc">FortiShield that brilliant and practical tool, is developed to assist users in efficiently handling the often-cramped storage space of their Android mobile phones. By virtue of its robust and innovative functions, it can meticulously take care of your device's complex file system, numerous apps as well as precious data, and at the same time offer you a truly straightforward and highly efficient experience.</p>
+    <div class="wrap-header-bg">
+      <div class="wrap-header-bg-c">
+        <div class="header">
+          <div class="header-l">
+            <img :src="ImgHL" alt="">
+          </div>
+          <div class="header-r">
+            <p @click="open('terms')">Terms</p>
+            <span class="line">|</span>
+            <p @click="open('privacy')">Privacy</p>
+          </div>
         </div>
-        <img class="rig" src="@/assets/image/cont.png">
+        <div class="content">
+          <img class="content-t" :src="ImgHCT" alt="" srcset="">
+          <p class="content-des">
+            Pure Manager that brilliant and practical tool, is developed to assist users in efficiently handling the often-cramped storage space of their Android mobile phones. 
+          </p>
+          <img class="content-b" :src="ImgHCB" alt="">
+        </div>
       </div>
     </div>
-    <div class="wrap-bot">
-      <img class="bImg" src="@/assets/image/bottom.png">
+    <div class="wrap-con">
+      <div class="wrap-con-qc">
+        <div class="wrap-con-qc-h">
+          <img :src="ImgWQcH">
+          <p class="wrap-con-qc-h-des">Saves you time with a quick and easy search.</p>
+        </div>
+        <ul class="wrap-con-qc-c">
+          <li class="wrap-con-qc-c-li">
+            <img :src="ImgWCLi1">
+            <p>Saves you time with a quick and easy search.Saves you time with a quick and easy search.</p>
+          </li>
+          <li class="wrap-con-qc-c-li">
+            <img :src="ImgWCLi2">
+            <p>Saves you time with a quick and easy search.Saves you time with a quick and easy search.</p>
+          </li>
+        </ul>
+        <div class="wrap-con-qc-b">
+          <img :src="ImgWQcB" alt="">
+        </div>
+      </div>
     </div>
+    <div class="wrap-b"></div>
     <div class="foot">
-      <div class="foot-h">
-        <p @click="open('terms')">Terms</p>
-        <span class="line">|</span>
-        <p @click="open('privacy')">Privacy</p>
+      <div class="foot-c">
+        <img class="foot-c-l" :src="ImgLogoB">
+        <div class="foot-c-r">
+          <div class="foot-c-r-h">
+            <p @click="open('privacy')">Privacy</p>
+            <span class="line">|</span>
+            <p @click="open('terms')">Terms</p>
+          </div>
+          <a class="email" href="mailto:info.abedok@gmail.com">
+            Contact us：info.abedok@gmail.com
+          </a>
+        </div>
       </div>
-      <a class="email" href="mailto:info.abedok@gmail.com">
-        Contact us：info.abedok@gmail.com
-      </a>
     </div>
   </div>
 </template>
@@ -44,74 +88,144 @@ const open = (type) => {
   margin: 0;
   padding: 0;
 }
+ul, li{
+  list-style: none;
+}
 .wrap{
   width: 100%;
   height: 100%;
   min-height: 100vh;
-  &-con{
-    min-height: calc(100vh - 88px - 92px);
-    overflow-y: auto;
-    position: relative;
-    &-top{
-      margin-bottom: 148px;
-      display: flex;
-      justify-content: flex-end;
-      img{
-        width: 689px;
-        margin-right: 10%;
-      }
-    }
-    &-t{
-      display: flex;
-      justify-content: center;
-      gap: 32px;
-      margin-bottom: 62px;
-      .left{
-        width: 524px;
-        .graph{
-          width: 80px;
-          height: 80px;
-          flex-shrink: 0;
-          border-radius: 12px;
-          // background: #40BA6D;
-          margin-bottom: 40px;
-          margin-top: 72px;
-        }
-        .tit{
-          color: #000000;
-          font-family: "SF Pro Display";
-          font-size: 40px;
-          font-style: normal;
-          // font-weight: 800;
-          margin-bottom: 20px;
-          display: flex;
-          span{
-            margin-left: 10px;
-            // height: 56px;
-            flex-shrink: 0;
-            border-radius: 12px;
-            background: #0ebe5a5c;
-            padding: 4px 8px;
+  &-header-bg{
+    background-size: 100% 100%;
+    height: 1080px;
+    background-image: url(@/assets/image/h-bg.png);
+    &-c{
+      width: 1200px;
+      margin: 0 auto;
+      .header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 19px 4px;
+        &-l{
+          width: 210px;
+          img{
+            width: 100%;
           }
         }
-        .desc{
+        &-r{
+          display: flex;
           color: #999999;
-          // font-family: "Roboto";
+          text-align: center;
+          font-family: "Roboto";
           font-size: 16px;
           font-style: normal;
-          // font-weight: 400;
-          line-height: 24px;
+          font-weight: 400;
+          line-height: normal;
+          text-decoration-line: underline;
+          text-decoration-style: solid;
+          text-decoration-skip-ink: none;
+          text-decoration-thickness: auto;
+          text-underline-offset: auto;
+          text-underline-position: from-font;
+          cursor: pointer;
+          .line{
+            color: #999999;
+            text-align: center;
+            font-family: "Roboto";
+            font-size: 14px;
+            font-style: normal;
+            font-weight: 400;
+            line-height: normal;
+            opacity: 0.5;
+            margin: 0 16px;
+          }
         }
       }
-      .rig{
-        width: 620px;
-        height: auto;
+      .content{
+        margin-top: 105px;
+        &-t{
+          height: 84px;
+          img{
+            height: 100%;
+          }
+        }
+        &-des{
+          margin: 18px 0 26px;
+          color: #a8b3aa;
+          text-align: center;
+          font-family: "SF Pro Display";
+          font-style: normal;
+          font-weight: 500;
+          line-height: 36px;
+          font-size: 24px;
+        }
+        &-b{
+          width: 100%;
+          img{
+            width: 100%;
+          }
+        }
       }
     }
-    &-b{
-      display: flex;
-      height: 92px;
-    }
+  }
+  &-con{
+    width: 1200px;
+    margin: 166px auto;
+    &-qc{
+      &-h{
+        img{
+          height: 96px;
+        }
+        &-des{
+          margin-top: 8px;
+          color: #a8b3aa;
+          font-family: "SF Pro Display";
+          font-size: 32px;
+          font-style: normal;
+          font-weight: 500;
+          line-height: 36px;
+        }
+      }
+      &-c{
+        margin-top: 77px;
+        display: flex;
+        gap: 50px;
+        margin-bottom: 264px;
+        &-li{
+          width: 550px;
+          flex-shrink: 0;
+          border-radius: 32px;
+          border: 1.5px solid #D8D8D8;
+          padding: 27px 27px 33px 27px;
+          word-wrap: break-word;
+          color: #666666;
+          font-family: "SF Pro Display";
+          font-size: 32px;
+          font-style: normal;
+          font-weight: 500;
+          line-height: 36px;
+          img{
+            width: 80px;
+            height: 80px;
+            margin-bottom: 27px;
+          }
+
+        }
+      }
+      &-b{
+        width: 100%;
+        img{
+          width: 100%;
+        }
+      }
+    }  
+  }
+  &-b{
+    margin-top: -520px;
+    height: 470px;
+    flex-shrink: 0;
+    background: #5BC771;
   }
   &-bot{
     height: 92px;
@@ -125,43 +239,53 @@ const open = (type) => {
   height: 88px;
   flex-shrink: 0;
   background-color: #000;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-  &-h{
-    color: #ffffff;
-    text-align: center;
-    font-family: "Roboto";
-    font-size: 14px;
-    font-style: normal;
-    font-weight: 400;
-    line-height: normal;
-    margin-bottom: 14px;
-    p{
-      text-decoration-line: underline;
-      text-decoration-style: solid;
-      text-decoration-skip-ink: none;
-      text-decoration-thickness: auto;
-      text-underline-offset: auto;
-      text-underline-position: from-font;
-      cursor: pointer;
-    }
+  &-c{
+    height: 100%;
+    margin: 0 auto;
+    width: 1200px;
     display: flex;
-    .line{
-      margin: 0 16px;
-      text-decoration: unset;
+    justify-content: space-between;
+    align-items: center;
+    &-l{
+      width: 204px;
     }
-  }
-  .email{
-    color: #ffffff;
-    text-align: center;
-    font-family: "Roboto";
-    font-size: 14px;
-    font-style: normal;
-    font-weight: 400;
-    line-height: normal;
-    text-decoration: none;
+    &-r{
+      &-h{
+        color: rgba(255, 255, 255, 0.7);
+        text-align: right;
+        font-family: "Roboto";
+        font-size: 14px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+        margin-bottom: 14px;
+        p{
+          text-decoration-line: underline;
+          text-decoration-style: solid;
+          text-decoration-skip-ink: none;
+          text-decoration-thickness: auto;
+          text-underline-offset: auto;
+          text-underline-position: from-font;
+          cursor: pointer;
+        }
+        display: flex;
+        flex-direction: row-reverse;
+        .line{
+          margin: 0 16px;
+          text-decoration: unset;
+        }
+      }
+      .email{
+        color: rgba(255, 255, 255, 0.7);
+        text-align: right;
+        font-family: "Roboto";
+        font-size: 14px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+        text-decoration: none;
+      }
+    }
   }
 }
 </style>
